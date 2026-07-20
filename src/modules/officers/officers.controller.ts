1e9f3d5ece38dd7489975175821dad6158c6687d
usifu <usifubarriem@gmail.com>
2026-07-20 15:49:04 +0000

    env files

--- src/modules/officers/officers.controller.ts
+++ src/modules/officers/officers.controller.ts
@@ -65,7 +65,7 @@ export class OfficersController {
   }
 
   @Patch('me')
-  @Permissions('officer:read:self')
+  @Permissions('profile:update:self')
   @ApiOperation({
     summary: 'Update current officer contact details and emergency contacts',
   })
